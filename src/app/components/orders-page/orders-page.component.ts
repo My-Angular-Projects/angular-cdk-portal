6b371920535515cf53f804b1b4b6efff0ddaee65
Valery Semenenko <gearmobile@gmail.com>
2024-06-04 14:44:04 +0300

    finish example

--- src/app/components/orders-page/orders-page.component.ts
+++ src/app/components/orders-page/orders-page.component.ts
@@ -1,6 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 import { ORDERS_DATA } from '@shared/data';
+import { CdkPortal } from '@angular/cdk/portal';
+import { PortalBridgeService } from '@shared/services';
 
 @Component({
   selector: 'pr-orders-page',
@@ -8,7 +17,19 @@ import { ORDERS_DATA } from '@shared/data';
   styleUrl: './orders-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OrdersPageComponent {
+export class OrdersPageComponent implements OnInit, OnDestroy {
+  private readonly service = inject(PortalBridgeService);
   public readonly displayedColumns = ['item', 'address', 'country'];
   public readonly dataSource = ORDERS_DATA;
+
+  @ViewChild(CdkPortal, { static: true })
+  private readonly portalContent!: CdkPortal;
+
+  ngOnInit(): void {
+    this.service.setPortal(this.portalContent);
+  }
+
+  ngOnDestroy(): void {
+    this.portalContent.detach();
+  }
 }
